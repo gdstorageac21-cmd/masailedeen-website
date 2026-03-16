80e4bd9e08e3cff62e8b6e9967379d3ea1f33be9
Your Name <you@example.com>
2026-03-16 16:03:07 +0500

    Fix Liquid template processing for HTML files

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
+    templateFormats: ["html", "md", "njk", "liquid"],
+    htmlTemplateEngine: "liquid",
+    markdownTemplateEngine: "liquid",
     dir: {
       input: "src",
       output: "_site",
